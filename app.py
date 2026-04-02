44768fcb85e0771f8787543551d90cebe3f42a69
mabdullah12345 <156498102+mabdullah12345@users.noreply.github.com>
2026-04-02 09:03:15 +0500

    Fixed port for Railway

--- app.py
+++ app.py
@@ -165,4 +165,6 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, port=port, host='0.0.0.0')
+
